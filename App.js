e89dad2ae7cf92fc7b342fefe8997e59497029aa
BreadByteMogul <ama@kobyconrad.com>
2023-12-09 05:43:39 -0500

    nav bar as its own component

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import NavBar from "./NavBar";
 
 const Stack = createNativeStackNavigator();
 
@@ -22,97 +23,6 @@ export default function App() {
         />
       </Stack.Navigator>
     </NavigationContainer>
-  );
-}
-
-function NavBar({ navigation }) {
-  return (
-    <View
-      style={{
-        width: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "red",
-        flexDirection: "row",
-      }}
-    >
-      {/* <Button title="Details" onPress={() => navigation.navigate("Details")} /> */}
-      <TouchableOpacity
-        style={{
-          backgroundColor: "yellow",
-          width: "25%",
-          height: 80,
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-        onPress={() => navigation.navigate("Home")}
-      >
-        <View style={{ marginBottom: 20 }}>
-          <Text>H</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={{
-          backgroundColor: "pink",
-          width: "25%",
-          height: 80,
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-        onPress={() => {
-          navigation.navigate("Details");
-          console.log("go details plz");
-        }}
-      >
-        <View style={{ marginBottom: 20 }}>
-          <Text>D</Text>
-        </View>
-      </TouchableOpacity>
-      <View
-        style={{
-          backgroundColor: "orange",
-          width: "25%",
-          height: 80,
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <View style={{ marginBottom: 20 }}>
-          <Text>S</Text>
-        </View>
-      </View>
-      <View
-        style={{
-          backgroundColor: "purple",
-          width: "25%",
-          height: 80,
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <View style={{ marginBottom: 20 }}>
-          <Text>S</Text>
-        </View>
-      </View>
-      <View
-        style={{
-          backgroundColor: "cyan",
-          width: "25%",
-          height: 80,
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <View style={{ marginBottom: 20 }}>
-          <Text>S</Text>
-        </View>
-      </View>
-    </View>
   );
 }
 
---
+++ NavBar.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+function NavBar({ navigation }) {
+  return (
+    <View
+      style={{
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "red",
+        flexDirection: "row",
+      }}
+    >
+      {/* <Button title="Details" onPress={() => navigation.navigate("Details")} /> */}
+      <TouchableOpacity
+        style={{
+          backgroundColor: "yellow",
+          width: "25%",
+          height: 80,
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        onPress={() => navigation.navigate("Home")}
+      >
+        <View style={{ marginBottom: 20 }}>
+          <Text>H</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{
+          backgroundColor: "pink",
+          width: "25%",
+          height: 80,
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        onPress={() => {
+          navigation.navigate("Details");
+          console.log("go details plz");
+        }}
+      >
+        <View style={{ marginBottom: 20 }}>
+          <Text>D</Text>
+        </View>
+      </TouchableOpacity>
+      <View
+        style={{
+          backgroundColor: "orange",
+          width: "25%",
+          height: 80,
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <View style={{ marginBottom: 20 }}>
+          <Text>S</Text>
+        </View>
+      </View>
+      <View
+        style={{
+          backgroundColor: "purple",
+          width: "25%",
+          height: 80,
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <View style={{ marginBottom: 20 }}>
+          <Text>S</Text>
+        </View>
+      </View>
+      <View
+        style={{
+          backgroundColor: "cyan",
+          width: "25%",
+          height: 80,
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <View style={{ marginBottom: 20 }}>
+          <Text>S</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default NavBar;
